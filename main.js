bd3d1b1e9ffce60e34eaa4e8a71f9f116a7bda35
dwnorm2 <dwnorm2@gmail.com>
2023-01-02 17:21:03 -0600

    add resolution input field and isolate functions

--- main.js
+++ main.js
@@ -2,50 +2,64 @@ const gridContainer = document.querySelector(".grid-container");
 
 let row;
 let square;
-let resolution = 50;
+let resolution = 16;
 
+//change res
+function changeRes() {
+  gridContainer.innerHTML = "";
+  resolution = document.getElementById("myInput").value;
+  if (resolution < 2) {
+    resolution = 2;
+  } else if (resolution > 100) {
+    resolution = 100;
+  }
+  getPixelSize();
+  createGrid();
+  squareCustomization();
+}
+
+//caluclate pixel size
 function getPixelSize() {
   return 500 / resolution;
 }
 
-const pixelSize = getPixelSize();
+//grid creation
+function createGrid() {
+  for (var i = 0; i < resolution; i++) {
+    row = document.createElement("div");
+    row.classList.add("square-row");
+    gridContainer.appendChild(row);
 
-for (var i = 0; i < resolution; i++) {
-  row = document.createElement("div");
-  row.classList.add("square-row");
-  gridContainer.appendChild(row);
-
-  for (var x = 0; x < resolution; x++) {
-    square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
+    for (var x = 0; x < resolution; x++) {
+      square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
   }
 }
 
+//random color
 function random_bg_color() {
-  let x = Math.floor(Math.random() * 256);
-  let y = Math.floor(Math.random() * 256);
-  let z = Math.floor(Math.random() * 256);
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
   return "rgb(" + x + "," + y + "," + z + ")";
 }
 
-const randomColor = random_bg_color();
+//square customization
+function squareCustomization() {
+  let pixelSize = getPixelSize();
+  document.querySelectorAll(".square").forEach((square) => {
+    square.setAttribute("style", `width:${pixelSize}px`);
+    square.setAttribute("style", `height:${pixelSize}px`);
 
-document.querySelectorAll(".square").forEach((square) => {
-  square.setAttribute("style", `width:${pixelSize}px`);
-  square.setAttribute("style", `height:${pixelSize}px`);
-
-  function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    return "rgb(" + x + "," + y + "," + z + ")";
-  }
-
-  const randomColor = random_bg_color();
-
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = randomColor;
-    // square.style.backgroundColor = "black";
+    square.addEventListener("mouseover", () => {
+      let randomColor = random_bg_color();
+      square.style.backgroundColor = randomColor;
+      // square.style.backgroundColor = "black";
+    });
   });
-});
+}
+
+createGrid();
+squareCustomization();
