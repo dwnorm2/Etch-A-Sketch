d526bc25976232c623a0a1773152d8730a8fa9a3
dwnorm2 <dwnorm2@gmail.com>
2023-01-01 18:53:14 -0600

    Add JS

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+const gridContainer = document.querySelector(".grid-container");
 
+let row;
+let square;
+let resolution = 16;
+
+// function getHeight() {
+//   return;
+// }
+
+for (var i = 0; i < resolution; i++) {
+  row = document.createElement("div");
+  row.classList.add("square-row");
+  gridContainer.appendChild(row);
+
+  for (var x = 0; x < resolution; x++) {
+    square = document.createElement("div");
+    square.classList.add("square");
+    row.appendChild(square);
+  }
+}
+
+document.querySelectorAll(".square").forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "black";
+  });
+});
